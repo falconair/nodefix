9d6170397e08454f8bfaa20c6cafbef9293a4cf7
Shahbaz Chaudhary <shahbazc@gmail.com>
2010-09-18 23:27:57 -0400

    added utility methods

--- fix.js
+++ fix.js
@@ -55,6 +55,37 @@ net.createServer(function(stream){
     stream.on("end", function(){pipeline.pushIncoming({eventType:"end"});}); 
 });
 
+//Utility methods
+var tag2txt = function(msg){ return Object.keys(msg).map(function(key){return tags[key]+"="+msg[key];}).join("|");}
+
+logger.format = function(level, timestamp, message) {
+  return [timestamp.getUTCFullYear() ,"/", timestamp.getUTCMonth() ,"/", timestamp.getUTCDay() , "-" , timestamp.getUTCHours() , ":" , timestamp.getUTCMinutes() , ":" , timestamp.getUTCSeconds() , "." , timestamp.getUTCMilliseconds() , " [" , level, "] ",  message].join("");
+};
+
+function checksum(str){
+    var chksm = 0;
+    for(var i=0; i<str.length; i++){
+        chksm += str.charCodeAt(i);
+    }
+    
+    chksm = chksm % 256;
+    
+    var checksumstr = "";
+    if (chksm < 10) {
+        checksumstr = "00" + chksm;
+    }
+    else if (chksm >= 10 && chksm < 100) {
+        checksumstr = "0" + chksm;
+    }
+    else {
+        checksumstr = "" + chksm;
+    }
+    
+    return checksumstr;
+}
+
+//Protocol handlers
+
 function FIXFrameDecoder(){
     this.description = "fix frame decoder: accepts raw text, creates messages";
     this.databuffer = "";
