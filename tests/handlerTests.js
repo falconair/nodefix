fef82183ad5255e27a9aade83c8caa953e6547e4
Shahbaz Chaudhary <shahbazc@gmail.com>
2010-10-15 00:09:18 -0400

    bug fixes

--- tests/handlerTests.js
+++ tests/handlerTests.js
@@ -55,17 +55,18 @@ var tests = {
         
         //console.log(sys.inspect(pipeline));
         
-        pipeline.addHandler({incoming:function(ctx,evt){console.log("in1:"+evt.data); ctx.sendNext(evt); } });
+        //pipeline.addHandler({incoming:function(ctx,evt){console.log("in1:"+evt.data); ctx.sendNext(evt); } });
         //pipeline.addHandler({outgoing:function(ctx,evt){console.log("out1:"+evt.data); ctx.sendNext(evt); } });
-        //pipeline.addHandler(FIXMsgDecoder.makeFIXMsgDecoder(FIX42));
-        pipeline.addHandler({incoming:function(ctx,evt){console.log("in2:"+evt.data); ctx.sendNext(evt); } });
+        pipeline.addHandler(FIXMsgDecoder.makeFIXMsgDecoder(FIX42));
+        //pipeline.addHandler({incoming:function(ctx,evt){console.log("in2:"+evt.data); ctx.sendNext(evt); } });
         //pipeline.addHandler({outgoing:function(ctx,evt){console.log("out2:"+evt.data); ctx.sendNext(evt); } });
-        //pipeline.addHandler(FIXMsgValidator.makeFIXMsgValidator(FIX42));
+        pipeline.addHandler(FIXMsgValidator.makeFIXMsgValidator(FIX42));
         //pipeline.addHandler({incoming:function(ctx,evt){console.log("in3:"+evt.data); ctx.sendNext(evt); } });
         
         pipeline.pushIncomingData(fix);
-        console.log(pipeline.state);
-        console.log(pipeline.state.senderCompID);
+        
+        //console.log(pipeline.state);
+        //console.log(pipeline.state.senderCompID);
         assert.equal( pipeline.state.senderCompID, "SENDER" );
     },
 
