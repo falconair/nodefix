dde165c2ffee145be4ea73cb06c1faff4b308adf
Shahbaz Chaudhary <shahbazc@gmail.com>
2011-01-03 22:08:53 -0500

    changed sessionHandler to FIX

--- fixClient.js
+++ fixClient.js
@@ -1,4 +1,4 @@
-var fix = require("./sessionHandler.js");
+var fix = require("./fix.js");
 
 var session = fix.createConnection("FIX.4.2", "initiator", "acceptor", 56000, "localhost");
 session.on("connect", function(){ console.log("connected"); });
--- fixServer.js
+++ fixServer.js
@@ -1,16 +1,4 @@
-/*net = require('net');
-
-net.createServer(function(stream){
-
-    this.session = null;
-    
-    stream.on("connect", function() {this.session = require("./sessionHandler.js").makeSessionHandler(stream, true)} );
-    stream.on("data",  function(data){ this.session.onData(data) });
-
-}).listen(56000, "localhost");
-*/
-//test
-var fix = require('./sessionHandler.js');
+var fix = require('./fix.js');
 fix.createServer(function(session){
     session.on("logon", function(id){ console.log(id + " logged in"); });
     session.on("data", function(data){ console.log("Message: "+data); });
