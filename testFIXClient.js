16c75ebfecb83a7677e2f426b87b20afeb2a4878
Shahbaz Chaudhary <shahbazc@gmail.com>
2011-02-05 20:51:05 -0500

    removed useless comments

--- testFIXClient.js
+++ testFIXClient.js
@@ -6,31 +6,6 @@ session.on("connect", function(){ console.log("EVENT connect"); });
 session.on("end", function(){ console.log("EVENT end"); });
 session.on("logon", function(sender, target){ console.log("EVENT logon: "+ sender + ", " + target); });
 session.on("logoff", function(sender, target){ console.log("EVENT logoff: "+ sender + ", " + target); });
-session.on("data", function(data){ console.log("EVENT data: "+ JSON.stringify(data)); });
 session.on("incomingmsg", function(data){ console.log("EVENT incomingmsg: "+ JSON.stringify(data)); });
 session.on("outgoingmsg", function(data){ console.log("EVENT outgoingmsg: "+ JSON.stringify(data)); });
-//session.write({...});
 
-/*
-var net = require('net');
-var pipe = require('./lib/nodepipe.js');
-
-var port = 1234;
-var host = 'localhost';
-
-var fixVersion = 'FIX.4.2';
-var targetCompID = 'TARGET';
-var senderCompID = 'SENDER';
-
-
-var stream = net.createConnection(port, host);
-
-stream.on('connect', function() {
-    var p = pipe.makePipe(stream);
-    p.addHandler(require('./handlers/fixFrameDecoder.js').newFixFrameDecoder());
-    p.addHandler(require('./handlers/logonManager.js').newLogonManager(true));
-    p.addHandler(require('./handlers/sessionProcessor.js').newSessionProcessor(true));
-    
-    p.pushOutgoing({'8': fixVersion, '56': targetCompID, '49': senderCompID, '35': 'A', '90': '0', '108': '30'});
-});
-*/
--- testFIXServer.js
+++ testFIXServer.js
@@ -5,26 +5,8 @@ fix.createServer(function(session){
     session.on("connect", function(){ console.log("EVENT connect"); });
     session.on("end", function(sender,target){ console.log("EVENT end"); });
     session.on("logon", function(sender, target){ console.log("EVENT logon: "+ sender + ", " + target); });
-    session.on("data", function(data){ console.log("EVENT data: "+ JSON.stringify(data)); });
     session.on("incomingmsg", function(data){ console.log("EVENT incomingmsg: "+ JSON.stringify(data)); });
     session.on("outgoingmsg", function(data){ console.log("EVENT outgoingmsg: "+ JSON.stringify(data)); });
 
 }).listen(1234, "localhost");
 
-/*
-var net = require('net');
-var pipe = require('./lib/nodepipe.js');
-
-net.createServer(function(stream) {
-
-
-        stream.on('connect', function() {
-            var p = pipe.makePipe(stream);
-            p.addHandler(require('./handlers/fixFrameDecoder.js').newFixFrameDecoder());
-            p.addHandler(require('./handlers/logonManager.js').newLogonManager(false));
-            p.addHandler(require('./handlers/sessionProcessor.js').newSessionProcessor(false));
-        });
-
-
-}).listen(1234,'localhost');
-*/
