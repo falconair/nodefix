676a5f3a8eeec31bf14750c4c5e9bfd8d23221c4
Shahbaz Chaudhary <shahbazc@gmail.com>
2011-05-26 00:51:46 -0400

    moves new sess proc 2 code to fix.js

--- fix.js
+++ fix.js
@@ -51,48 +51,9 @@ function Server(func) {
             session.p = pipe.makePipe(stream);
             session.p.addHandler(require('./handlers/fixFrameDecoder.js').newFixFrameDecoder());
             session.p.addHandler(require('./handlers/outMsgEvtInterceptor.js').newOutMsgEvtInterceptor(session));
-            session.p.addHandler(require('./handlers/logonProcessorAcceptor.js').newlogonProcessorAcceptor());
+            session.p.addHandler(require('./handlers/sessionProcessor2.js').newSessionProcessor(true));
             session.p.addHandler(require('./handlers/inMsgEvtInterceptor.js').newInMsgEvtInterceptor(session));
-            session.p.addHandler(require('./handlers/sessionProcessor.js').newSessionProcessor(false));
             
-            //session constants
-            ctx.state.session['isInitiator'] = false;
-            ctx.state.session['remoteAddress'] = ctx.stream.remoteAddress;
-
-            //session defaults (may be over-written from inside the pipe)
-            ctx.state.session['heartbeatDuration'] = 30;
-            ctx.state.session['testRequestID'] = 1;
-            ctx.state.session['isLoggedIn'] = false;
-            ctx.state.session['isResendRequested'] = false;
-            
-            //session variables, set inside pipe, not here
-            //ctx.state.session['fixVersion'] = fixVersion;
-            //ctx.state.session['senderCompID'] = senderCompID;
-            //ctx.state.session['targetCompID'] = targetCompID;
-            //ctx.state.session['incomingSeqNum'] = incomingSeqNum;
-            //ctx.state.session['outgoingSeqNum'] = outgoingSeqNum;
-            //ctx.state.session['heartbeatDuration'] = parseInt(heartbeatInMilliSeconds,10) * 1000;
-            //ctx.state.session['timeOfLastIncoming'] = new Date().getTime();
-            //ctx.state.session['timeOfLastOutgoing'] = new Date().getTime();
-
-            /*session.p.addHandler({incoming:function(ctx,event){
-                if(event.type === 'session' && event.data === 'logon'){
-                    session.senderCompID = ctx.state.session.senderCompID;
-                    session.targetCompID = ctx.state.session.targetCompID;
-                    session.fixVersion = ctx.state.session.fixVersion;
-
-                    session.sessionEmitter.emit('logon',ctx.state.session.senderCompID,ctx.state.session.targetCompID);
-                }
-                else if(event.type === 'session' && event.data === 'logoff'){
-                    session.sessionEmitter.emit('logoff',ctx.state.session.senderCompID,ctx.state.session.targetCompID);
-                }
-                else if(event.type==='error'){
-                    session.sessionEmitter.emit('error', event.data);
-                }
-                
-                ctx.sendNext(event);
-
-            }});*/
             
         });
         stream.on('data', function(data) { session.p.pushIncoming({data:data, type:'data'}); });
@@ -147,41 +108,9 @@ function Client(fixVersion, senderCompID, targetCompID) {
     self.p = pipe.makePipe(stream);
     self.p.addHandler(require('./handlers/fixFrameDecoder.js').newFixFrameDecoder());
     self.p.addHandler(require('./handlers/outMsgEvtInterceptor.js').newOutMsgEvtInterceptor(session));
-    self.p.addHandler(require('./handlers/logonProcessorInitiator.js').newlogonProcessorInitiator());
-    self.p.addHandler(require('./handlers/inMsgEvtInterceptor.js').newInMsgEvtInterceptor(session));
-    self.p.addHandler(require('./handlers/sessionProcessor.js').newSessionProcessor(true));
-
-    //Set pipeliene's constants. All outgoing messages are auto-filled with this information
-    //session constants
-    self.p.state.session['fixVersion'] = fixVersion;
-    self.p.state.session['senderCompID'] = senderCompID;
-    self.p.state.session['targetCompID'] = targetCompID;
-    self.p.state.session['isInitiator'] = true;
-    
-    //session defaults (may be over-written from inside the pipe)
-    self.p.state.session['testRequestID'] = 1;
-    self.p.state.session['isLoggedIn'] = false;
-    self.p.state.session['isResendRequested'] = false;
-    self.p.state.session['incomingSeqNum'] = 1;
-    self.p.state.session['outgoingSeqNum'] = 1;
-    
-    //session values set inside pipe, not here
-    //self.p.state.session['heartbeatDuration'] = heartbeatInMilliSeconds;
-    //self.p.state.session['timeOfLastOutgoing'] = timeOfLastOutgoing;
-    //self.p.state.session['timeOfLastIncoming'] = timeOfLastIncoming;
-
-    /*this.p.addHandler({incoming:function(ctx,event){ 
-        if(event.type==='session' && event.data==='logon'){
-            self.emit('logon', ctx.state.session.senderCompID, ctx.state.session.targetCompID);
-        }
-        else if(event.type==='session' && event.data==='logoff'){
-            self.emit('logoff', ctx.state.session.senderCompID, ctx.state.session.targetCompID);
-        }
-        else if(event.type==='error'){
-            self.emit('error', event.data);
-        }
-        ctx.sendNext(event); 
-    }});*/
+    self.p.addHandler(require('./handlers/sessionProcessor2.js').newSessionProcessor(false));
+   self.p.addHandler(require('./handlers/inMsgEvtInterceptor.js').newInMsgEvtInterceptor(session));
+ 
     
     stream.on('connect', function() {
         self.emit('connect');
